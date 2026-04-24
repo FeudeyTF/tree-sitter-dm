4e7611a89ef0ad573a4cd1ff0e5924819d2db049
FeudeyTF <yanmordanenko089@gmail.com>
2026-04-24 12:40:55 +0300

    Added tags for type definition and reference

--- queries/tags.scm
+++ queries/tags.scm
@@ -1,6 +1,13 @@
 (proc_definition
   name: (identifier) @name) @definition.function
 
+(type_definition
+  (type_path 
+    (type_identifier) @name)) @definition.type
+
+(type_path_expression 
+  (type_identifier) @name) @reference.type
+
 (call_expression
   name: [
       (identifier) @name
